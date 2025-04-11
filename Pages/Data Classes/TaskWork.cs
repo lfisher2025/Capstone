e04a3eff8fc91a6e72bda166ef938b80fd4707ce
davis8le@dukes.jmu.edu <davis8le@dukes.jmu.edu>
2025-04-10 22:17:35 -0400

    Added task functinality

---
+++ Pages/Data Classes/TaskWork.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab1.Pages.Data_Classes
+{
+    public class TaskWork
+    {
+        public int TaskID { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime Deadline { get; set; }
+        public string Status { get; set; }
+        public int AssignedTo { get; set; } // UserID of the user assigned to the task
+        public int ProjectId { get; set; } // ProjectID of the project the task belongs to
+        public int GrantApplicationID { get; set; } // GrantApplicationID of the grant application the task is related to
+
+    }
+}
--- Pages/Task.cshtml.cs
+++ Pages/Task.cshtml.cs
@@ -1,12 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Lab1.Pages.Data_Classes;
+using Lab1.Pages.DB;
+using System.Data.SqlClient;
 
 namespace Lab1.Pages
 {
     public class TaskModel : PageModel
     {
+        public List<TaskWork> TaskInfo { get; set; } = new List<TaskWork>();
+        
+        public TaskModel()
+        {
+            TaskInfo = new List<TaskWork>();
+        }
         public void OnGet()
         {
+            SqlDataReader taskReader = DBClass.TaskReader(); // Replace with the actual user ID
+            while (taskReader.Read())
+            {
+                TaskWork task = new TaskWork
+                { 
+                    TaskName = taskReader.GetString(1),
+                    TaskDescription = taskReader.GetString(2),
+                    Deadline = taskReader.GetDateTime(3),
+                    Status = taskReader.GetString(4),
+                    AssignedTo = taskReader.GetInt32(5),
+                    ProjectId = taskReader.GetInt32(6),
+                    GrantApplicationID = taskReader.GetInt32(7)
+                };
+                TaskInfo.Add(task);
+            }
+            DBClass.Lab1DBConnection.Close();
         }
     }
 }
